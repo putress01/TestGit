26a32f2be5589d58c2f02423e8e5f881395e2038
zhao_kuo <zhao_kuo@outlook.com>
2021-04-14 13:43:53 +0800

    more edit...

--- src/main/java/com/putress/git/Good.java
+++ src/main/java/com/putress/git/Good.java
@@ -1,5 +1,9 @@
 package com.putress.git;
 
-public class Good {
+import com.sun.tools.javac.launcher.Main;
 
+public class Good {
+	public static void main(String[] args) {
+		System.out.print("Good!!!");
+	}
 }
